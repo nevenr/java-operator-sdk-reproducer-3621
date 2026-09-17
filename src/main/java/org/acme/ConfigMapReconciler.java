555d7a29a45dc2bb5ae7ec63ffb68d13fe195cd2
Neven Radovanović <neven.radovanovic@gmail.com>
2026-09-17 14:17:52 +0200

    Initial commit

---
+++ src/main/java/org/acme/ConfigMapReconciler.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import org.jboss.logging.Logger;
+
+@ControllerConfiguration(triggerReconcilerOnAllEvents = true, generationAwareEventProcessing = false)
+public class ConfigMapReconciler implements Reconciler<ConfigMap> {
+    private static final Logger LOG = Logger.getLogger(ConfigMapReconciler.class);
+
+    @Override
+    public UpdateControl<ConfigMap> reconcile(ConfigMap resource, Context<ConfigMap> context) throws Exception {
+        LOG.infof("Certificate %s/%s event", resource.getMetadata().getNamespace(), resource.getMetadata().getName());
+
+        if (context.isPrimaryResourceDeleted()) {
+            LOG.infof("deleted");
+            return UpdateControl.noUpdate();
+        }
+
+        LOG.infof("created/updated");
+        return UpdateControl.noUpdate();
+    }
+}
